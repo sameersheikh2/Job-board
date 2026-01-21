b897433f62dad53bd3df9870dada9f3f59ad3934
Sameer Sheikh <sameersheikh819@gmail.com>
2026-01-21 19:26:20 +0530

    user model added

--- backend/src/models/User.js
+++ backend/src/models/User.js
@@ -1,3 +1,44 @@
 const mongoose = require("mongoose");
+const validator = require("validator");
+const bcrypt = require("bcryptjs");
 
-const User = new mongoose.Schema({});
+const UserSchema = new mongoose.Schema({
+  name: { type: String, required: true, trim: true, minlength: 4 },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    trim: true,
+    lowercase: true,
+    validate: {
+      validator: validator.isEmail,
+      message: "Invalid email format",
+    },
+  },
+  password: {
+    type: String,
+    required: true,
+    select: false,
+    minlength: 6,
+    validate: {
+      validator: validator.isStrongPassword,
+      message: "Password is not strong enough",
+    },
+  },
+  role: { type: String, enum: ["job_seeker", "recruiter"], required: true },
+  isVerified: { type: Boolean, default: false },
+  createdAt: { type: Date, default: Date.now },
+});
+
+UserSchema.pre("save", async function () {
+  if (!this.isModified("password")) {
+    return;
+  }
+  this.password = await bcrypt.hash(this.password, 10);
+});
+
+UserSchema.methods.comparePassword = async function (userPassword) {
+  return await bcrypt.compare(userPassword, this.password);
+};
+
+module.exports = mongoose.model("User", UserSchema);
